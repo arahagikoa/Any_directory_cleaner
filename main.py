ce8d0f0d87eaa90756b199d984db2dab3281365f
arahagikoa <krzeminski.kamil@interia.eu>
2023-09-09 00:16:50 +0200

    finally working :partially:

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-import os 
-import shutil 
+import os
+import shutil
+
+desktop = os.path.expanduser(r"C:\Users\kmlkr\OneDrive\Pulpit")
+
+list_of_exts_n_dirs = {
+    "pdf": r"C:\Users\kmlkr\OneDrive\Pulpit\Stuff\pdfs",
+    "lnk": r"C:\Users\kmlkr\OneDrive\Pulpit\Stuff\short",
+    "exe": r"C:\Users\kmlkr\OneDrive\Pulpit\Stuff\exe",
+    "txt": r"C:\Users\kmlkr\OneDrive\Pulpit\Stuff\otheroffice"
+}
+
+for dir_path in list_of_exts_n_dirs.values():
+    os.makedirs(dir_path, exist_ok=True)
 
 
-desktop = os.path.expanduser(r"C:\Users\kmlkr\OneDrive\Pulpit\Stuff\pdfs")
-pdf_dir = os.path.expanduser(r"C:\Users\kmlkr\OneDrive\Pulpit\Stuff\pdfs")
-
-for dir_path in [pdf_dir]:
-    os.makedirs(dir_path, exist_ok = True)
-
-
-def move_files_by_extension(src_dir, dest_dir, extensions):
+def move_files_by_extension(src_dir, dest_dir):
     for filename in os.listdir(src_dir):
         file_path = os.path.join(src_dir, filename)
         if os.path.isfile(file_path):
             file_extension = filename.split(".")[-1].lower()
-            if file_extension in extensions:
-                dest_path = os.path.join(dest_dir, filename)
-                shutil.move(file_path, dest_path)
-                print (f"Moved {filename} to {dest_dir}")
+            for extension, dir_path in list_of_exts_n_dirs.items():
+                if file_extension == extension:
+                    dest_path = os.path.join(dir_path, filename)
+                    shutil.move(file_path, dest_path)
+                    print(f"Moved {filename} to {dir_path}")
 
 
-pdf = ["pdf"]
-move_files_by_extension(desktop, pdf_dir, pdf)
+for extension, dest_dir in list_of_exts_n_dirs.items():
+    move_files_by_extension(desktop, dest_dir)
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
 import os
 import shutil
+
+desktop = os.path.expanduser(r"C:\Users\kmlkr\OneDrive\Pulpit")
+pdf_dir = os.path.expanduser(r"C:\Users\kmlkr\OneDrive\Pulpit\Stuff\pdfs")
+
+print(os.listdir(desktop))
