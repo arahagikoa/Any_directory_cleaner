5baf11c03a2e5a805042b26983222d8b8a3ab9d9
arahagikoa <krzeminski.kamil@interia.eu>
2023-09-12 18:12:45 +0200

    unziping

--- unzip_file.py
+++ unzip_file.py
@@ -6,4 +6,21 @@ from tkinter import filedialog
 #unzip file
 
 def unzip():
-    return "lol"
+    file = filedialog.askopenfilename()
+    extraction_directory = filedialog.askdirectory()
+    if "." in file:
+        file_extension = file.split(".")[-1]
+        commands = {
+            "zip": "unzip {} -d {}".format(file, extraction_directory),
+            "gz": "gunzip {} {}".format(file, extraction_directory),
+            "tar": "tar -xvf {} -C {}".format(file, extraction_directory),
+            "tar.gz": "tar -xzvf {} -C {}".format(file, extraction_directory),
+            "tar.bz2": "tar -xjvf {} -C {}".format(file, extraction_directory),
+            "7z": "7z x {} -o{}".format(file, extraction_directory)
+        }
+        for key, value in commands.items():
+            if file_extension == key:
+                result = subprocess.run(value, shell = True, stdout=subprocess.PIPE, text=True) 
+                return result.stdout
+
+unzip()
